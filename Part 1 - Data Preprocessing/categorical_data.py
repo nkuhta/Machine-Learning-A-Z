b8a4b5f38e2a10d8266dc7f146748cbcaed14a0b
nkuhta <nicholas.kuhta@gmail.com>
2018-11-05 21:19:51 -0800

    data preprocessing

--- Part 1 - Data Preprocessing/categorical_data.py
+++ Part 1 - Data Preprocessing/categorical_data.py
@@ -36,15 +36,3 @@ y = labelencoder_y.fit_transform(y)
 
 print(X)
 print(y)
-
-
-# # Encoding categorical data
-# # Encoding the Independent Variable
-# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# labelencoder_X = LabelEncoder()
-# X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-# onehotencoder = OneHotEncoder(categorical_features = [0])
-# X = onehotencoder.fit_transform(X).toarray()
-# # Encoding the Dependent Variable
-# labelencoder_y = LabelEncoder()
-# y = labelencoder_y.fit_transform(y)
